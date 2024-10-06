7399f35f5c8c8df17682f448324f77ba3d933f8c
albinols <albinolsson@live.com>
2024-10-06 15:51:19 +0200

    progran.cs updated

--- SignalRChatAppV2.Server/Program.cs
+++ SignalRChatAppV2.Server/Program.cs
@@ -1,30 +1,102 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using SignalRChatAppV2.Server.Data.Contexts;
+using SignalRChatAppV2.Server.Data.Entities;
+using SignalRChatAppV2.Server.Hubs;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+
+builder.Services.AddDbContext<ChatContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("ChatDbConnection")));
+
+builder.Services.AddDefaultIdentity<UserEntity>(x =>
+{
+    x.User.RequireUniqueEmail = true;
+    x.SignIn.RequireConfirmedEmail = false;
+    x.Password.RequiredLength = 8;
+
+}).AddEntityFrameworkStores<ChatContext>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+        builder.WithOrigins("https://localhost:5174")
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials());
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("d5df9b30d5891d6e19c3eda79aef6fa0181cb5f0da195f2bbb54022c7d217b1b"))
+        };
+
+
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+
+                var accessToken = context.Request.Query["access_token"];
+
+
+                if (!string.IsNullOrEmpty(accessToken) && context.HttpContext.Request.Path.StartsWithSegments("/chathub"))
+                {
+                    context.Token = accessToken;
+                }
+
+                return Task.CompletedTask;
+            }
+        };
+    });
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5052);
+    options.ListenAnyIP(7128, listenOptions =>
+    {
+        listenOptions.UseHttps();
+    });
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapFallbackToFile("/index.html");
 app.MapControllers();
 
-app.MapFallbackToFile("/index.html");
+app.MapHub<ChatHub>("/chathub");
 
 app.Run();
